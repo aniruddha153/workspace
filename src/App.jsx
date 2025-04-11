b03c5c1afe3674bc7143a2d845f0b0df6f4c96f3
Aniruddha Pondhe <aniruddha153@gmail.com>
2025-04-11 02:29:29 +0000

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      <h2>This is Aniruddha. How are you?</h2>
+      <h2>This is Aniruddha.</h2>
     </div>
   );
 }
